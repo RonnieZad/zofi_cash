0d8aba6852c17da4e31b7e9b1cb11933da0c6142
Ronnie Zad <zadcorna@gmail.com>
2022-07-09 17:14:24 +0300

    updated pager test

--- test/screens/pager_test.dart
+++ test/screens/pager_test.dart
@@ -4,7 +4,7 @@ import 'package:zofi_cash/controllers/pager_controller.dart';
 
 void main() {
   final pageController = Get.put(PagerController());
-  group('test page shuffler', () {
+  group('test page shuffler,', () {
     test('check current index init value', () {
       expect(pageController.currentIndex.value, 0);
     });
