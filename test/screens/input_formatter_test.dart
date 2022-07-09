6f1a2f4a63bdb1f9e5a010b93fd4e6a00a73d8e1
Ronnie Zad <zadcorna@gmail.com>
2022-07-09 17:14:02 +0300

    updated input formatter test

--- test/screens/input_formatter_test.dart
+++ test/screens/input_formatter_test.dart
@@ -2,22 +2,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zofi_cash/utils/utils.dart';
 
 main() {
-  group('Test Text Format', () {
-    test('Test text formatter when there is no user input', () {
+  group('Test Text Format,', () {
+    test('when there is no user input', () {
       var digit1 = Helper.getTextDigit('');
       expect(digit1, "0");
     });
-    test('Test text formatter when user enters random value > 1000', () {
+    test('when user enters random value > 1000', () {
       var digit1 = Helper.getTextDigit('34555');
       expect(digit1, "34,555");
     });
 
-    test('Test text formatter when user enters random value < 1000', () {
+    test('when user enters random value < 1000', () {
       var digit2 = Helper.getTextDigit('231');
       expect(digit2, "231");
     });
 
-    test('Test text formatter when user enters decimal value', () {
+    test('when user enters decimal value', () {
       var digit3 = Helper.getTextDigit('1645.367');
       expect(digit3, "1,645.367");
     });
