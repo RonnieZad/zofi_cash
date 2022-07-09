a2e17a7f797d9c1c0ea971f3e3d654116cea36e7
Ronnie Zad <zadcorna@gmail.com>
2022-07-09 17:12:01 +0300

    updated insert text function

--- test/screens/home_page_test.dart
+++ test/screens/home_page_test.dart
@@ -4,14 +4,15 @@ import 'package:zofi_cash/controllers/home_page_controller.dart';
 
 void main() {
   final homePageController = Get.put(HomePageController());
-  test(
-    'initial value should be empty',
-    () {
-      expect(homePageController.amountController.value.text, '');
-    },
-  );
 
-  group('test insertText function', () {
+  group('test insertText function,', () {
+    test(
+      'initial value should be empty',
+      () {
+        expect(homePageController.amountController.value.text, '');
+      },
+    );
+
     test('check whether insert text is called and text is inserted 1', () {
       homePageController.insertText('1');
       expect(homePageController.amountController.value.text, '1');
